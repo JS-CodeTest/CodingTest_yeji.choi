d34dc93cbece0cde95cc504b182521bdf8064173
yeji.choi <moojinkl@gmail.com>
2023-04-17 17:17:30 +0900

    수업시간 풀이 | 0417

---
+++ Programmers_Lv.0/23.04.17 잘라서 배열로 저장하기.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ my_str의 길이 ≤ 100
+// 1 ≤ n ≤ my_str의 길이
+// my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
+
+function solution(my_str, n) {
+  let reg = new RegExp(`.{1,${n}}`, "g");
+  return my_str.match(reg);
+}
