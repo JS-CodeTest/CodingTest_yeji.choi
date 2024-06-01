f729df8060c574a08484aca796d5b884af2956ea
yeji.choi <moojinkl@gmail.com>
2024-06-01 22:37:25 +0900

    덧셈식 출력하기 | 0601

---
+++ Programmers_Lv.0/24.06.01 덧셈식 출력하기.js
@@ -0,0 +1,21 @@
+// 문제 설명
+// 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+// a + b = c
+
+// 제한사항
+// 1 ≤ a, b ≤ 100
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const [first, second] = input.map((a) => Number(a));
+  console.log(`${first} + ${second} = ${first + second}`);
+});
