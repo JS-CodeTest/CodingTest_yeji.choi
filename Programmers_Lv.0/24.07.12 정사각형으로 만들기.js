4eb1a45409c4a07b0bcdeda2bac91ae86490c852
yeji.choi <moojinkl@gmail.com>
2024-07-12 23:49:40 +0900

    정사각형으로 만들기 | 0712

---
+++ Programmers_Lv.0/24.07.12 정사각형으로 만들기.js
@@ -0,0 +1,21 @@
+// 문제 설명
+// 이차원 정수 배열 arr이 매개변수로 주어집니다. arr의 행의 수가 더 많다면 열의 수가 행의 수와 같아지도록 각 행의 끝에 0을 추가하고, 열의 수가 더 많다면 행의 수가 열의 수와 같아지도록 각 열의 끝에 0을 추가한 이차원 배열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ arr의 길이 ≤ 100
+// 1 ≤ arr의 원소의 길이 ≤ 100
+// arr의 모든 원소의 길이는 같습니다.
+// 1 ≤ arr의 원소의 원소 ≤ 1,000
+
+function solution(arr) {
+  const max = Math.max(arr.length, arr[0].length);
+
+  for (let i = 0; i < max; i++) {
+    arr[i] = arr[i] || [];
+    while (arr[i].length < max) {
+      arr[i].push(0);
+    }
+  }
+
+  return arr;
+}
